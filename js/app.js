677166dde99397adc00986cadb9edf842edebdfc
ADmoises <moi.prado@duocuc.cl>
2022-07-26 21:15:14 -0400

    guardando data de los inputs en objeto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,53 @@
 document.addEventListener('DOMContentLoaded', startApp);
 
+const events = [];
+
+const eventTemplate = {
+  days: '',
+  name: '',
+  date: '',
+}
+
 function startApp(){
+  createEvent();
+}
+
+function createEvent(){
+  const $eventsContainer = document.querySelector('#eventos');
+  const $btnEvent = document.querySelector('#btn-evento');
+
+  addEventInfo();
+
+  $btnEvent.addEventListener('click', e => {
+  });
+}
+
+function addEventInfo() {
+  saveInputData('#nombre-evento', 'name');
+  saveInputData('#fecha', 'date');
+  
 
 }
+
+function saveInputData(element = '',dataTosave = '') {
+  document.addEventListener('input', e => {
+    if (e.target.matches(element)) {
+      eventTemplate[dataTosave] = e.target.value;
+      console.log(eventTemplate);
+    }
+  });
+}
+
+function addEventDayLeft(){
+  
+}
+
+
+
+function addEventDate(){
+  
+}
+
+
 
 
